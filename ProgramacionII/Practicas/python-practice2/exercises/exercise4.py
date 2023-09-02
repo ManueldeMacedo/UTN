0da19aea4c68f71dd25802a7bf0929e74fa233a1
ManueldeMacedo <manuel_demacedo@hotmail.com>
2023-09-02 15:43:32 -0300

    Practicas Programacion 2

---
+++ ProgramacionII/Practicas/python-practice2/exercises/exercise4.py
@@ -0,0 +1,84 @@
+"""Expresiones Booleanas."""
+
+
+def es_vocal_if(letra: str) -> bool:
+    """Toma un string y devuelve un booleano en base a si letra es una vocal o
+    no.
+
+    Restricciónes:
+        - Utilizar un if para cada posibilidad.
+        - Utilizar la función lower() sólo una vez.
+        - No utilizar ELSE.
+        - Utilizar 6 returns.
+
+    Referencia: https://docs.python.org/3/library/stdtypes.html#string-methods
+    """
+
+    letra=letra.lower()
+    if(letra=="a"):
+        return True
+    elif(letra=='e'):
+        return True
+    elif(letra=='i'):
+        return True
+    elif(letra=='o'):
+        return True
+    elif(letra=='u'):
+        return True
+
+# NO MODIFICAR - INICIO
+assert es_vocal_if("a")
+assert not es_vocal_if("b")
+assert es_vocal_if("A")
+assert es_vocal_if("e")
+assert es_vocal_if("E")
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def es_vocal_if_in(letra: str) -> bool:
+    """Re-escribir utilizando un sólo IF y el operador IN.
+
+    Restricciónes:
+        - Utilizar un único IF.
+        - Utilizar dos returns.
+        - No utilizar ELSE.
+        - No utilizar FOR.
+        - No utilizar listas.
+
+    Referencia: https://docs.python.org/3/reference/expressions.html#membership-test-operations # noqa: E501
+    """
+
+    letra=letra.lower()
+    if(letra in ('a', 'e', 'i', 'o', 'u')):
+        return True
+
+# NO MODIFICAR - INICIO
+assert es_vocal_if_in("a")
+assert not es_vocal_if_in("b")
+assert es_vocal_if_in("A")
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def es_vocal_in(letra: str) -> bool:
+    """Re-escribir como expresión booleana utilizando el operador IN
+
+    Restricciónes:
+        - No utilizar IF.
+        - Utilizar un único return.
+        - No utilizar FOR.
+        - No utilizar listas.
+    """
+
+    return letra.lower() in ('a', 'e', 'i', 'o', 'u')
+
+# NO MODIFICAR - INICIO
+assert es_vocal_in("a")
+assert not es_vocal_in("b")
+assert es_vocal_in("A")
+# NO MODIFICAR - FIN
